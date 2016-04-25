d7bdfba508e0108d8766f729619401a879b1fb0e
黄飞 <huangfei@huangfeidemac-mini.local>
2016-04-25 15:00:56 +0800

    Initial Commit

---
+++ 二维码测试/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  二维码测试
+//
+//  Created by 黄飞 on 16/4/25.
+//  Copyright © 2016年 黄飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
